e8de22c8cc588f0b2ced14de4381b15d69b54a9e
ze211311 <ze.narongkorn_st@tni.ac.th>
2023-10-13 20:48:08 +0700

    Another Final

--- src/Component/modal_Appoint.js
+++ src/Component/modal_Appoint.js
@@ -77,7 +77,6 @@ const Modal_appoint = () => {
               </Table.Body>
             </Table>
             <div className="w-full">
-              <Button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">Register</Button>
             </div>
           </div>
         </Modal.Body>
--- src/Component/modal_Vacc.js
+++ src/Component/modal_Vacc.js
@@ -69,8 +69,6 @@ const Modal_Vacc = () => {
               </Table.Body>
             </Table>
             <div className="w-full">
-              <Button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
-              onClick={()=>{}}>Register</Button>
             </div>
           </div>
         </Modal.Body>
